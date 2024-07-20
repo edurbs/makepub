b313e7fa03b3d6cc8feadb37954056d431395f0d
Eduardo Soares <edurbs@gmail.com>
2024-07-20 17:38:58 -0300

    add repository and mapper

---
+++ src/main/java/com/company/makepub/entity/repository/MarkupRepository.java
@@ -0,0 +1,7 @@
+package com.company.makepub.entity.repository;
+
+import com.company.makepub.entity.Markup;
+import io.jmix.core.repository.JmixDataRepository;
+
+public interface MarkupRepository extends JmixDataRepository<Markup, String> {
+}
--- src/main/java/com/company/makepub/view/converter/Converter.java
+++ src/main/java/com/company/makepub/view/converter/Converter.java
@@ -4,17 +4,19 @@ package com.company.makepub.view.converter;
 import com.company.makepub.app.domain.MarkupRecord;
 import com.company.makepub.app.usecase.ConvertText;
 import com.company.makepub.app.usecase.myUUIDGenerator;
-import com.company.makepub.entity.Markup;
+import com.company.makepub.entity.mapper.MarkupMapper;
+import com.company.makepub.entity.repository.MarkupRepository;
 import com.company.makepub.view.main.MainView;
 import com.vaadin.flow.component.ClickEvent;
 import com.vaadin.flow.router.Route;
-import io.jmix.core.DataManager;
 import io.jmix.flowui.component.textarea.JmixTextArea;
 import io.jmix.flowui.kit.component.button.JmixButton;
 import io.jmix.flowui.view.*;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 @Route(value = "Converter", layout = MainView.class)
 @ViewController("Converter")
@@ -25,29 +27,17 @@ public class Converter extends StandardView {
     @ViewComponent
     private JmixTextArea textAreaOutput;
     @Autowired
-    private DataManager dataManager;
+    private MarkupRepository markupRepository;
 
     @Subscribe(id = "convertButton", subject = "clickListener")
     public void onConvertButtonClick(final ClickEvent<JmixButton> event) {
         String inputText = textAreaInput.getValue();
-        List<MarkupRecord> markupRecordList = dataManager.load(Markup.class).all().list()
-                .stream().map(
-                    m -> new MarkupRecord(
-                            m.getId(),
-                            m.getHtmlStart(),
-                            m.getHtmlEnd(),
-                            m.getIsParagraph(),
-                            m.getIsFootnoteSymbol(),
-                            m.getIsFootnoteText(),
-                            m.getIsQuestion(),
-                            m.getCreatedBy()
-                    )
-            ).toList();
-        String outputText = new ConvertText(new myUUIDGenerator(), markupRecordList).convert(inputText);
+        List<MarkupRecord> markupRecordList = new ArrayList<>();
+        markupRepository.findAll()
+                .forEach(m -> markupRecordList.add(
+                        new MarkupMapper().to(m)));
+        String outputText = new ConvertText(new myUUIDGenerator(), markupRecordList)
+                .convert(inputText);
         textAreaOutput.setValue(outputText);
     }
-
-
-
-
 }
