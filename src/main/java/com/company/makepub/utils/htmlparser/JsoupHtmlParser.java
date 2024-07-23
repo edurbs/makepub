a3cac87b8e069b3d50264dc49b8c07a5d2566272
Eduardo Soares <edurbs@gmail.com>
2024-07-23 17:25:08 -0300

    fix read text between two html tags

--- src/main/java/com/company/makepub/utils/htmlparser/JsoupHtmlParser.java
+++ src/main/java/com/company/makepub/utils/htmlparser/JsoupHtmlParser.java
@@ -4,36 +4,115 @@ import com.company.makepub.app.gateway.HtmlParser;
 import com.company.makepub.app.usecase.exceptions.UseCaseException;
 import jakarta.validation.constraints.NotNull;
 import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
+import org.jsoup.nodes.*;
+import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.List;
 
 public class JsoupHtmlParser implements HtmlParser {
 
     @Override
     public String parse(@NotNull String site, @NotNull String tag) throws UseCaseException {
-        String tagContent = "";
+        Document doc = getDocument(site);
+        Element tagElement = doc.getElementById(tag);
+        if(tagElement == null) {
+            return "";
+        }
+        return getTagContent(tagElement.wholeText());
+
+    }
+
+    private  String getTagContent(String tagContent) {
+        if(tagContent.isBlank()) {
+            return "";
+        }
+        String surroundInTag = "p";
+        String startSurround = "<" + surroundInTag + ">";
+        String endSurround = "</" + surroundInTag + ">";
+        StringBuilder sb = new StringBuilder();
+        String[] lines = tagContent.split("\n");
+        for(String line : lines) {
+            if(!line.isBlank()){
+                sb.append(startSurround);
+                sb.append(line.trim());
+                sb.append(endSurround);
+                sb.append("\n");
+            }
+        }
+        return sb.toString();
+    }
+
+    private static @NotNull Document getDocument(String site) {
         Document doc;
         try {
             doc = Jsoup.connect(site).get();
         } catch (IOException e) {
             throw new UseCaseException("Erro ao conectar ao site " + site, e);
         }
-        Element tagElement = doc.getElementById(tag);
-        if(tagElement != null) {
-            tagContent = tagElement.wholeText();
-            StringBuilder sb = new StringBuilder();
-            String[] lines = tagContent.split("\n");
-            for(String line : lines) {
-                if(!line.isBlank()){
-                    sb.append("<p>");
-                    sb.append(line.trim());
-                    sb.append("</p>\n");
+        return doc;
+    }
+
+    // TODO test
+    @Override
+    public String getTextBetweenTagId(String site, String tagIdStart, String tagIdEnd) {
+        Document doc = getDocument(site);
+        StringBuilder extractedText = new StringBuilder();
+        Element content = doc.getElementById("content");
+        if(content == null) {
+            return "";
+        }
+        List<Node> allNodes = content.childNodes();
+        boolean isTextExtractionActive = false;
+        for (Node node : allNodes) {
+            if (node instanceof Element) {
+                isTextExtractionActive =analyzeNode(tagIdStart, tagIdEnd, node, isTextExtractionActive, extractedText);
+                if(node.childNodeSize()>0){
+                    List<Node> childNodes = node.childNodes();
+                    for (Node childNode : childNodes) {
+                         isTextExtractionActive = analyzeNode(tagIdStart, tagIdEnd, childNode, isTextExtractionActive, extractedText);
+                    }
                 }
             }
-            tagContent = sb.toString();
         }
-        return tagContent;
+        return extractedText.toString().replaceAll("\\s+", " ").trim();
     }
+
+    private boolean analyzeNode(String tagIdStart, String tagIdEnd, Node node, boolean isTextExtractionActive, StringBuilder extractedText) {
+        isTextExtractionActive = checkIsTextExtractionActive(node, tagIdStart, tagIdEnd, isTextExtractionActive);
+        String textFromNode = readNode(node, isTextExtractionActive);
+        extractedText.append(textFromNode);
+       return isTextExtractionActive;
+    }
+
+    private String readNode(Node node, boolean isTextExtractionActive) {
+        return switch (node) {
+            case Element element when isTextExtractionActive && isScope(element) -> element.outerHtml();
+            case TextNode textNode when isTextExtractionActive -> textNode.outerHtml();
+            case null, default -> " ";
+        };
+    }
+
+    private boolean isScope(Element element){
+        return !element.tagName().equals("a");
+    }
+
+    private boolean checkIsTextExtractionActive(Node node, String tagIdStart, String tagIdEnd, boolean isTextExtractionActive) {
+        return switch (node) {
+          case Element element ->  {
+              String nodeId = element.id();
+              if(nodeId.equals(tagIdStart)) {
+                  yield true;
+              } else if(nodeId.equals(tagIdEnd)) {
+                  yield false;
+              }
+              yield isTextExtractionActive;
+          }
+          case null, default -> isTextExtractionActive;
+        };
+    }
+
+
+
+
 }
--- src/test/java/com/company/makepub/utils/htmlparser/JsoupHtmlParserTest.java
+++ src/test/java/com/company/makepub/utils/htmlparser/JsoupHtmlParserTest.java
@@ -8,19 +8,30 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class JsoupHtmlParserTest {
 
+    public final JsoupHtmlParser sut = new JsoupHtmlParser();
+
     @Test
     @DisplayName("Should parse the wol.jw.org page and return the tag content")
     void parse() {
-        JsoupHtmlParser jsoupHtmlParser = new JsoupHtmlParser();
-        String tagContent = jsoupHtmlParser.parse("https://wol.jw.org/xav/wol/d/r511/lp-xv/1102016802", "article");
+        String tagContent = sut.parse("https://wol.jw.org/xav/wol/d/r511/lp-xv/1102016802", "article");
         assertTrue(tagContent.contains("Jeová — Atsitsi ĩwẽ"));
     }
 
     @Test
     @DisplayName("Should throws exception when site is unreachable")
     void parseWithWrongSite() {
-        JsoupHtmlParser jsoupHtmlParser = new JsoupHtmlParser();
-        assertThrows(UseCaseException.class, () -> jsoupHtmlParser.parse("https://none.none", ""));
+        assertThrows(UseCaseException.class, () -> sut.parse("https://none.none", ""));
+    }
+
+    @Test
+    @DisplayName("Should read text between two tags")
+    void getContentBetweenTags() {
+        String url = "https://www.scriptureearth.org/data/xav/sab/xav/xav-19-LUK-001.html";
+        String expected = """
+                <span class="v">10</span><span class="vsp">&nbsp;</span>Wedewati isadaze te te zata ré, te rowi idasi'ubumro norĩ hã rowaptẽrẽ za'ra ni.<span id="bookmarks10"></span>
+                """.trim();
+        String tagContent = sut.getTextBetweenTagId(url, "v10", "v11");
+        assertEquals(expected, tagContent);
     }
 
 
