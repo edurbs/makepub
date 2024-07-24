318e396a6156a1cfef88c1b9b382e98857aeea98
Eduardo Soares <edurbs@gmail.com>
2024-07-24 09:50:04 -0300

    remove unused imports

--- src/main/java/com/company/makepub/app/usecase/DetectScripture.java
+++ src/main/java/com/company/makepub/app/usecase/DetectScripture.java
@@ -2,7 +2,6 @@ package com.company.makepub.app.usecase;
 
 import com.company.makepub.app.domain.Book;
 import com.company.makepub.app.domain.ScriptureAddress;
-import com.company.makepub.app.usecase.exceptions.UseCaseException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,7 +9,6 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class DetectScripture {
-
 
     public List<ScriptureAddress> execute(String html) {
         List<ScriptureAddress> result = new ArrayList<>();
@@ -32,47 +30,4 @@ public class DetectScripture {
             result.add(new ScriptureAddress(book, chapter, 0));
         }
     }
-
-    private Matcher detectBook(String html) {
-
-        return null;
-    }
-
-    /*
-    private int detectChaper(Matcher matcherBook, String html) {
-        int chapter= 0;
-        String collon = ";";
-        int bookEnd = matcherBook.end();
-        // read html until collon
-        int endIndex = bookEnd - collon.length() + 1;
-        if(endIndex> matcherBook.group().length()) {
-            return 0;
-        }
-        String htmlUntilCollon = matcherBook.group().substring(0, endIndex);
-        String regex = "(\\d+)";
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcherChapter = pattern.matcher(htmlUntilCollon);
-        if(matcherChapter.find()) {
-            this.chapter = Integer.parseInt(matcherChapter.group());
-        }
-        return matcherChapter.end();
-    }
-
-    private int detectStartVerse(int chapterEnd, String html) {
-        String endVerse = ")";
-        int indexEndVerse = html.indexOf(endVerse, chapterEnd);
-        // get last number using regex
-        String regex = "(\\d+)";
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(html.substring(chapterEnd, indexEndVerse));
-        if(matcher.find()) {
-            this.startVerse = Integer.parseInt(matcher.group(0));
-        }
-        //this.startVerse = Integer.parseInt(html.substring(chapterEnd, indexEndVerse));
-        return indexEndVerse;
-    }*/
-
-
-
-
 }
