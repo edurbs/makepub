f00b22211ae192990014062ae29f7efe12e40378
Eduardo Soares <edurbs@gmail.com>
2024-08-12 18:25:17 -0300

    add todo

--- src/main/java/com/company/makepub/app/usecase/types/BibleReader.java
+++ src/main/java/com/company/makepub/app/usecase/types/BibleReader.java
@@ -1,6 +1,6 @@
 package com.company.makepub.app.usecase.types;
 
 public interface BibleReader {
-    String getScripture(String book, int chapter, int verse);
+    String getScripture(String book, int chapter, int verse); // TODO chave String book to ENUM
     String getScripture(String book, int chapter, int startVerse, int endVerse);
 }
