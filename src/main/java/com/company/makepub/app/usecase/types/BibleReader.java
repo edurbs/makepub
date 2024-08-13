9a589b9ea271a8b724005f306f5ad4d9afd15b3b
Eduardo Soares <edurbs@gmail.com>
2024-08-13 20:42:36 -0300

    add method getScripture with record parameter

--- src/main/java/com/company/makepub/app/usecase/types/BibleReader.java
+++ src/main/java/com/company/makepub/app/usecase/types/BibleReader.java
@@ -1,6 +1,9 @@
 package com.company.makepub.app.usecase.types;
 
+import com.company.makepub.app.domain.ScriptureAddress;
+
 public interface BibleReader {
     String getScripture(String book, int chapter, int verse); // TODO chave String book to ENUM
     String getScripture(String book, int chapter, int startVerse, int endVerse);
+    String getScripture(ScriptureAddress scriptureAddress);
 }
