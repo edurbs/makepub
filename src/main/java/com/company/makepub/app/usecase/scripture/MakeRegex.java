484327976ebff8dc2d771a3f389baa6471533f30
Eduardo Soares <edurbs@gmail.com>
2024-08-13 20:42:55 -0300

    add text cleaner

--- src/main/java/com/company/makepub/app/usecase/scripture/MakeRegex.java
+++ src/main/java/com/company/makepub/app/usecase/scripture/MakeRegex.java
@@ -7,17 +7,28 @@ import java.util.regex.Pattern;
 
 public class MakeRegex {
     private final String text;
+    private Matcher matcher;
+    private static final Pattern pattern = Pattern.compile(getRegex());
 
     public MakeRegex (String text) {
         this.text = text;
+        execute();
     }
 
-    public Matcher execute(){
-        Pattern pattern = Pattern.compile(getRegex());
-        return pattern.matcher(text);
+    public Matcher getMatcher() {
+        return matcher;
     }
 
-    private String getRegex(){
+    private void execute(){
+        String cleanedText = cleanText(text);
+        matcher = pattern.matcher(cleanedText);
+    }
+
+    private String cleanText(String text) {
+        return text.replace("\u00A0", " "); // non-breaking space
+    }
+
+    private static String getRegex(){
         StringBuilder booksForRegex = new StringBuilder();
         for(Book book : Book.values()) {
             booksForRegex.append(book.getFullName()).append("|");
